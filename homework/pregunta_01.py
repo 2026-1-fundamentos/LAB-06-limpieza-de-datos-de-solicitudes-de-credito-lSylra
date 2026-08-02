079eaf0f94a40be47e719a01bc0ab2c2a6e77f5e
lSylra <esgallegoa@unal.edu.co>
2026-08-01 20:46:36 -0500

    Intento 1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -13,3 +13,27 @@ def pregunta_01():
     El archivo limpio debe escribirse en "files/output/solicitudes_de_credito.csv"
 
     """
+    import pandas as pd
+    import os
+
+    dataframe = pd.read_csv("files/input/solicitudes_de_credito.csv", index_col=0, sep=";")
+    dataframe.dropna(axis=0, how="any", inplace=True)
+
+    # Limpieza de las variables
+    dataframe["sexo"] = dataframe["sexo"].str.lower()
+    dataframe["monto_del_credito"] = (dataframe["monto_del_credito"].str.replace("$", "", regex=False).str.replace(",", "", regex=False).astype(float))
+    dataframe["fecha_de_beneficio"] = pd.to_datetime(dataframe["fecha_de_beneficio"],dayfirst=True, format="mixed", errors="coerce")
+
+    # Unificación de categorías
+    columnas_a_unificar = ["tipo_de_emprendimiento","idea_negocio","barrio","línea_credito"]
+
+    for columna in columnas_a_unificar:
+        dataframe[columna] = dataframe[columna].str.lower().str.replace(r"[ .-]", "_", regex=True).str.strip()
+
+    dataframe.drop_duplicates(inplace=True)
+
+    # Crear el directorio de salida si no existe
+    output_dir = "files/output"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    dataframe.to_csv("files/output/solicitudes_de_credito.csv", sep=";", header=True, index=False)
